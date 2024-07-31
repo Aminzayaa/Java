02f8f5ab7cd42737ff4e3d6160bbe2bc00cb753e
Aminzaya <tab.erd.aminzaya@gmail.com>
2024-07-31 17:41:36 +0800

    first commit

---
+++ java2_08_package/src/main/CallHello.java
@@ -0,0 +1,10 @@
+package main;
+
+public class CallHello {
+	public static void main(String[] args) {
+		// package a の Hello
+		a.Hello.printHello();
+		// package b の Hello
+		b.Hello.printHello();
+		}
+}
